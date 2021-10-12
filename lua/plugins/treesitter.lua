d9d345a355a46f91b961fe999a81c3c9b15e1f20
Vincent Breitmoser <look@my.amazin.horse>
2021-10-12 23:44:54 +0200

    enable treesitter function and class jump mappings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -68,6 +68,26 @@ require('nvim-treesitter.configs').setup({
 				['ic'] = '@class.inner',
 			},
 		},
+		move = {
+			enable = true,
+			set_jumps = false,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
 	},
 
 	-- See: https://github.com/JoosepAlviste/nvim-ts-context-commentstring
