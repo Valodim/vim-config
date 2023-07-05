026d8390d0d8cec57b55c0d6d74df04b53090fa1
Vincent Breitmoser <look@my.amazin.horse>
2023-07-05 10:06:16 +0200

    feat(keymaps): add wordmotion for M-w/e/b

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -1,3 +1,15 @@
 return {
   { 'tpope/vim-unimpaired', event = 'VeryLazy' },
+
+	{
+		'chaoren/vim-wordmotion',
+		keys = {
+			{ '<M-w>', '<Plug>WordMotion_w', mode = { 'n', 'x', 'o' } },
+			{ '<M-e>', '<Plug>WordMotion_e', mode = { 'n', 'x', 'o' } },
+			{ '<M-b>', '<Plug>WordMotion_b', mode = { 'n', 'x', 'o' } },
+		},
+		init = function()
+			vim.g.wordmotion_nomap = 1
+		end,
+	},
 }
