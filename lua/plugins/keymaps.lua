ca061a7b1f1b816ffb02a218e77c18f6d9b33515
Vincent Breitmoser <look@my.amazin.horse>
2023-07-05 10:06:56 +0200

    feat(keymaps): use vim-asterisk and nvim-hlslens for search commands

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -12,4 +12,36 @@ return {
 			vim.g.wordmotion_nomap = 1
 		end,
 	},
+
+	{
+		'kevinhwang91/nvim-hlslens',
+		keys = {
+			{ 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+			{ 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+			{ '<Leader><Space>', '<Cmd>nohlsearch<CR>' },
+		},
+		config = function ()
+			-- for some reason it's required to call this explicitly, using the init function wouldn't work
+			require('hlslens').setup()
+			vim.cmd[[
+				" toned down colors
+				highlight! link HlSearchLens Comment
+				highlight! link HlSearchLensNear Comment
+			]]
+		end,
+	},
+
+	{
+		'haya14busa/vim-asterisk',
+		dependencies = { 'kevinhwang91/nvim-hlslens' },
+		keys = {
+			{ '*', [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' } },
+			{ 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' } },
+			{ '#', [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' } },
+			{ 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' } },
+		},
+		init = function()
+			vim.g['asterisk#keeppos'] = 1
+		end,
+	},
 }
