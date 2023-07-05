bdf4f3a2ba52c7cb59cafa2d0189f24cc58a97a3
Vincent Breitmoser <look@my.amazin.horse>
2023-07-05 10:09:43 +0200

    feat!: disable a couple of rafi's plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  { 'olimorris/persisted.nvim', enabled = false },
+  { 'andymass/vim-matchup', enabled = false },
+  { 'folke/noice.nvim', enabled = false },
+  { 'williamboman/mason.nvim', enabled = false },
+  { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
+  {'echasnovski/mini.bracketed', enabled = false },
+}
